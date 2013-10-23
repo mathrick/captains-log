409ab6d8695d24f8218ca664ccd3d9b7261de44e
Maciej Katafiasz <mathrick@gmail.com>
2013-10-24 00:00:00 +0200

    Add LogItem DB context and initialiser

--- CaptainsLog/Global.asax.cs
+++ CaptainsLog/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using CaptainsLog.Models;
 
 namespace CaptainsLog
 {
@@ -23,6 +25,8 @@ namespace CaptainsLog
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            Database.SetInitializer<LogItemDBContext>(new DropCreateDatabaseIfModelChanges<LogItemDBContext>());
         }
     }
 }
