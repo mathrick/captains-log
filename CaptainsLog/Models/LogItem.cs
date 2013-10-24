da992c13f79f0fd73d5691ba6016171aba32ba5c
Maciej Katafiasz <mathrick@gmail.com>
2013-10-24 11:03:22 +0200

    Add LogItem.cs, which I forgot

---
+++ CaptainsLog/Models/LogItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CaptainsLog.Models
+{
+    public class LogItem
+    {
+    }
+
+    public class LogItemDBContext : DbContext
+    {
+        DbSet<LogItem> items { get; set; }
+    }
+}
