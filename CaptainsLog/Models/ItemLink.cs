ac83427cae834d2f840832f1866d7db17956b22f
Maciej Katafiasz <mathrick@gmail.com>
2013-10-24 12:48:06 +0200

    Add ItemLink

---
+++ CaptainsLog/Models/ItemLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaptainsLog.Models
+{
+    /// <summary>
+    /// ItemLink represents a link to a place where a specific LogItem can be paid for
+    /// </summary>
+    public class ItemLink
+    {
+    }
+}
