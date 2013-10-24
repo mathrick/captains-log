b35d666b50f46d77f56506d06b229456c0f164cd
Maciej Katafiasz <mathrick@gmail.com>
2013-10-24 11:30:34 +0200

    Configure routes to avoid having /Home/ in URLs

--- CaptainsLog/App_Start/RouteConfig.cs
+++ CaptainsLog/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace CaptainsLog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Home",
+                url: "{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
