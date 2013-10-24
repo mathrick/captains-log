f49a54e425ae2a14d7b9fd8d28e2f9f55bb92276
Maciej Katafiasz <mathrick@gmail.com>
2013-10-24 12:48:57 +0200

    Add basic model for LogItems

--- CaptainsLog/Models/LogItem.cs
+++ CaptainsLog/Models/LogItem.cs
@@ -6,8 +6,16 @@ using System.Data.Entity;
 
 namespace CaptainsLog.Models
 {
+    /// <summary>
+    /// LogItems are specific goods (such as a game, a book, or a film). 
+    /// They represent the generic concept of content (such as "Lord of the Rings"), 
+    /// without reference to specific instances like editions, formats, or particular copies.
+    /// </summary>
     public class LogItem
     {
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<ItemLink> links { get; set; }
     }
 
     public class LogItemDBContext : DbContext
